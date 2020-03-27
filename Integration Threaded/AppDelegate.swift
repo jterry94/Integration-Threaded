145a9df97cc95bce9d987f9c0ca209fc852880ca
Jeff Terry <terryj@iit.edu>
2020-03-27 13:00:15 -0500

    Initial Commit

---
+++ Integration Threaded/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Integration Threaded
+//
+//  Created by Jeff Terry on 3/27/20.
+//  Copyright © 2020 Jeff Terry. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
